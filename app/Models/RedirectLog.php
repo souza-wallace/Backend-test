e06d80a06458b0dbcdcaed0f0980964e8011c97f
souza-wallace <walizdesouza781@gmail.com>
2024-02-05 00:27:38 -0300

    change name column

--- app/Models/RedirectLog.php
+++ app/Models/RedirectLog.php
@@ -12,16 +12,17 @@ class RedirectLog extends Model
     use HasFactory;
 
     protected $fillable = [
-        'redirect_id', 'ip_address', 'user_agent', 'referer', 'query_params', 'access_time',
+        'redirect_id', 'ip_address', 'user_agent', 'referer', 'query_params', 'access_time', 'redirect_code',
     ];
 
-    public static function createLog($id, $data){
+    public static function createLog($id, $code, $data){
         self::create([
             'redirect_id' => $id,
+            'redirect_code' => $code,
             'ip_address' => $data['ip_address'],
             'user_agent' => $data['user_agent'],
             'referer' => $data['referer'],
-            'query_params' => $data['query_params'],
+            'query_params' => json_encode($data['query_params_request']),
             'access_time' => Carbon::now()
         ]);
 
