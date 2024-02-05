0996117bc81be41d53edeca5713f7b46533065d8
souza-wallace <walizdesouza781@gmail.com>
2024-02-05 00:29:58 -0300

    many changes in controller

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -4,78 +4,50 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
+
 use Validator;
+use Carbon\Carbon;
+
 use App\Models\Redirect;
 use App\Models\RedirectLog;
-use Illuminate\Support\Facades\Http;
-use Carbon\Carbon;
+
+use Vinkla\Hashids\Facades\Hashids;
+
 
 class RedirectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Redirect $redirect): object
     {
-        return 'all';
+        return response()->json([
+            "data" => $redirect->all()
+        ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request, Redirect $redirect)
+    public static function store(Request $request, Redirect $redirect)
     {
         $rules = [
             'url_destiny' => 'required|url',
         ];
 
-        $validator = Validator::make($request->all(), $rules);
+        $validateFields = self::validateFields($request);
+        if(count($validateFields)){ return $validateFields; }
 
-        if ($validator->fails()) {
-            response()->json(['errors' => $validator->errors()], 422);
-        }
+        $validateUrl = isset($request->url_destiny) ? self::validateUrl($request->url_destiny) : false;
+        if(count($validateUrl)){ return response()->json($validateUrl, 500); }
 
-        if ($request->url_destiny === $request->url()) {
-            return response()->json(['errors' => ['url_destiny' => ['A URL de destino não pode apontar para a própria aplicação.']]], 422);
-        }
-
-        if (!str_starts_with($request->url_destiny, 'https')) {
-            return response()->json(['errors' => ['url_destiny' => ['A URL de destino deve ser "https".']]], 422);
-        }
-
-        try {
-            $response = Http::head($request->url_destiny);
-            if ($response->status() !== 200) {
-                return response()->json(['errors' => ['url_destiny' => ['A URL de destino não retornou um status 200.']]], 422);
-            }
-        } catch (\Exception $e) {
-            return response()->json(['errors' => ['url_destiny' => ['A URL de destino não é válida ou não está acessível.']]], 422);
-        }
-
-        $redirect->status = "Ativo";
+        $redirect->status = "ativo";
         $redirect->url_destiny = $request->input('url_destiny');
-        $redirect->last_access = $request->input('last_access');
+        $redirect->last_access = Carbon::now();
         $redirect->save();
 
         return response()->json([
             'data' => $redirect,
-            'message' => 'Redirect criado com sucesso'
+            'message' => 'Redirect created with sucess'
         ], 201);
-
-        // Ou redirecionar para uma página específica
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Redirect $redirect)
     {
         if(!$redirect){
@@ -88,7 +60,7 @@ class RedirectController extends Controller
             'code' => $redirect->code,
             'status' => $redirect->status,
             'url_destiny' => $redirect->url_destiny,
-            'last_acess' => $redirect->last_acess,
+            'last_access' => $redirect->last_access,
             'created_at' => $redirect->created_at,
             'updated_at' => $redirect->updated_at
         ];
@@ -96,47 +68,34 @@ class RedirectController extends Controller
         return response()->json($data, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Redirect $redirect)
     {
-        $validate = $this->validateFields($request);
-        if($validate){
-            return $validate;
-        }
+        $validateFields = self::validateFields($request);
+        if(count($validateFields)){ return $validateFields; }
+
+        $validateUrl = $this->validateUrl($request);
+        if($validateUrl){ return $validateUrl; }
 
         $redirect->url_destiny = $request->input('url_destiny');
         $redirect->status = $request->input('status');
         $redirect->save();
 
-        // Retornar uma resposta adequada (você pode personalizar isso conforme necessário)
-        return response()->json(['message' => 'Redirecionamento atualizado com sucesso']);
+        return response()->json(['message' => 'Redirect updated with sucess']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Redirect $redirect)
+    public function destroy(Redirect $redirect): object
     {
 
         if (!$redirect) {
-            return response()->json(['message' => 'Registro não encontrado'], 404);
+            return response()->json(['message' => 'not found'], 404);
         }
 
         $redirect->delete();
 
-        return response()->json(['message' => 'Registro deletado com sucesso']);
+        return response()->json(['message' => 'Registro deleted with sucess']);
     }
 
-    public function getRequestData($request, $redirect)
+    public function getRequestData($request, $redirect): array
     {
         $ipAddress = $request->ip();
 
@@ -144,57 +103,70 @@ class RedirectController extends Controller
 
         $referer = $request->header('referer');
 
-        $urlParts = parse_url($redirect->url_destiny);
-        parse_str(isset($urlParts['query']), $queryParams);
+        $queryParamsRequest = $request->query();
 
         return [
             'ip_address' => $ipAddress,
             'user_agent' => $userAgent,
             'referer' => $referer,
-            'query_params' => count($queryParams) ? json_encode($queryParams) : null,
+            'query_params_request' => count($queryParamsRequest) ? $queryParamsRequest : null,
         ];
     }
 
-    public function validateFields($request){
-        $rules = [
-            'url_destiny' => 'required|url',
-        ];
-
-        $validator = Validator::make($request->all(), $rules);
-
-        if ($validator->fails()) {
-            $message = response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        return isset($message);
-    }
-
-    public function redirect(Redirect $redirect, Request $request){
+    public function redirect(Redirect $redirect, Request $request)
+    {
         $data = $this->getRequestData($request, $redirect);
-        return $data;
+        $queryParamsRequest = $data['query_params_request'];
 
+        $urlDestiny = self::generateRedirectUrl($queryParamsRequest, $redirect->url_destiny);
+        $code = Hashids::encode($redirect->id, 10);
+
+        // return $code;
         try {
-            RedirectLog::createLog($redirect->id, $data);
+            return RedirectLog::createLog($redirect->id, $code, $data);
         } catch (\Exception $e) {
-            return response()->json(['fail_log' => $e], 500);
+            \Log::error('Erro ao salvar Log:' . $e->getMessage());
         }
-
-        $response = Http::get($redirect->url_destiny);
-
+    
+        $response = Http::get($urlDestiny);
+    
         return $response;
     }
 
-    public function stats($id){
-        $redirect = Redirect::with('logs')->find($id);
+    public static function generateRedirectUrl($queryParamsRequest, $url_destiny){
+        $urlQueryParams = [];
     
-        $totalAccesses = $redirect->logs->count();
-        $uniqueIPs = $redirect->logs->pluck('ip')->unique()->count();
+        parse_str(parse_url($url_destiny, PHP_URL_QUERY), $urlQueryParams);
     
-        $topReferrers = $redirect->logs->groupBy('referer')->sortByDesc(function ($referer) {
+        if($queryParamsRequest){
+            $queryParamsRequest = array_filter($queryParamsRequest, function ($value) {
+                return isset($value);
+            });
+        }
+
+        $mergedQueryParams = array_merge($queryParamsRequest ?? [], $urlQueryParams);
+        
+        $urlWithoutQuery = strtok($url_destiny, '?');
+        $urlDestiny = $urlWithoutQuery . '?' . http_build_query($mergedQueryParams);
+
+        return $urlDestiny;
+    }
+
+    public function stats($code){
+        $logs = RedirectLog::where('code', $code)->get();
+
+        if (!$logs) {
+            return response()->json(['error' => 'Redirect Log not found'], 404);
+        }
+    
+        $totalAccesses = $logs->count();
+        $uniqueIPs = $logs->pluck('ip')->unique()->count();
+    
+        $topReferrers = $logs->groupBy('referer')->sortByDesc(function ($referer) {
             return $referer->count();
         })->take(5)->keys()->toArray();
     
-        $last10Days = $redirect->logs->groupBy('created_at')->take(10)->map(function ($dayLogs, $date) {
+        $last10Days = $logs->groupBy('created_at')->take(10)->map(function ($dayLogs, $date) {
             $total = $dayLogs->count();
             $unique = $dayLogs->pluck('ip')->unique()->count();
             return ['date' => $date, 'total' => $total, 'unique' => $unique];
@@ -209,4 +181,59 @@ class RedirectController extends Controller
     
         return $stats;
     }
+
+    public static function validateUrl($url) {
+        $errors = [];
+    
+        $currentUrl = URL::current();
+    
+        if ($currentUrl == $url) {
+            $errors[] = 'A URL de destino não pode apontar para a própria aplicação.';
+        }
+    
+        $urlComponents = parse_url($url);
+    
+        if (!$urlComponents || !isset($urlComponents['scheme']) || !isset($urlComponents['host'])) {
+            $errors[] = 'A URL de destino é inválida.';
+        } else {
+            if (!checkdnsrr($urlComponents['host'])) {
+                $errors[] = 'O DNS da URL de destino não é válido.';
+            }
+    
+            if ($urlComponents['scheme'] !== 'https') {
+                $errors[] = 'A URL de destino deve ser "https".';
+            }
+
+            $response = Http::head($url);
+            if ($response->status() !== 200) {
+                $errors[] = 'A URL de destino não retornou um status 200.';
+            }
+        }
+
+        if (isset($urlComponents['query'])) {
+            parse_str($urlComponents['query'], $queryParams);
+            if (isset($queryParams[""]) || in_array("", array_values($queryParams))) {
+                $errors[] = 'A URL de destino não pode ter parâmetros de consulta com chaves vazias.';
+            }
+        }
+    
+        return $errors;
+    }
+
+    public static function validateFields($request): array
+    {
+        $rules = [
+            'url_destiny' => 'required|url',
+        ];
+
+        $errors = [];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            $errors[] = $validator->errors();
+        }
+        return $errors;
+    }
+    
 }
